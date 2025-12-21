fda1bbde90fce94c35d1522f04e7625b58a7091d
Birkin James Diana <birkin.diana@gmail.com>
2025-12-20 22:05:40 -0500

    restores check for dotenv-path.

--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,8 @@ from dotenv import find_dotenv, load_dotenv
 
 ## load envars ------------------------------------------------------
 dotenv_path = pathlib.Path(__file__).resolve().parent.parent.parent / '.env'
-# assert dotenv_path.exists(), f'file does not exist, ``{dotenv_path}``'
-# load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=True), override=True)
-load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=False), override=True)
+assert dotenv_path.exists(), f'file does not exist, ``{dotenv_path}``'
+load_dotenv(find_dotenv(str(dotenv_path), raise_error_if_not_found=True), override=True)
 
 
 log = logging.getLogger(__name__)
